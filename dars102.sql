a72de8ff7144202de497518f2039fbb6d80092ba
Hamroali Safarov <safarovhamroali4@gmail.com>
2026-04-06 13:59:52 +0500

    Add files via upload

---
+++ dars102.sql
@@ -0,0 +1,38 @@
+--CREATE DATABASE library;
+
+-- CREATE TABLE Authors(
+--     author_id INT,
+--     first_name VARCHAR(20),
+--     last_name VARCHAR(20),
+--     year_of_birth DATE,
+--     State VARCHAR(15)
+-- );
+
+-- CREATE TABLE Genres(
+-- 	genre_id INT,
+-- 	Genre_name VARCHAR(20),
+-- 	Description VARCHAR(20)
+-- );
+
+-- CREATE TABLE Books(
+-- 	book_id INT,
+-- 	book_name VARCHAR(20),
+-- 	AuthoR_id VARCHAR(20),
+-- 	Year_of_publication DATE,
+-- 	Number_of_pages INT,
+-- 	Number_of_copies_available INT
+-- );
+
+
+CREATE TABLE Members(
+	members_id INT,
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	phone_number TEXT,
+	Date_of_registration DATE
+
+);
+
+
+
+
